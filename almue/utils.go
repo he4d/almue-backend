ebd5f702bf45fcc599e8b613e8a1507f94eebb82
Tobias Tschinkowitz <he4d@posteo.de>
2017-06-17 11:50:54 +0200

    allow all origins

--- almue/utils.go
+++ almue/utils.go
@@ -12,6 +12,7 @@ func respondWithError(w http.ResponseWriter, code int, message string) {
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	response, _ := json.Marshal(payload)
 
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(response)
